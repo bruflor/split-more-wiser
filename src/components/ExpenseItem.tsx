696c34afca095a5fc10b6bd92e63e41ee388b4b0
Bruna Flôr <brn.flor@gmail.com>
2022-06-03 09:20:49 +0100

    compenent card

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,5 +1,6 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 interface IPropsExpensive {
   id: string;
@@ -10,13 +11,13 @@ interface IPropsExpensive {
 
 function ExpenseItem(props: IPropsExpensive) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
