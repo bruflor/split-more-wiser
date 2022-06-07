9b6528693cbf6e26d3030f96c2755a146c8586ce
Bruna Flôr <brn.flor@gmail.com>
2022-06-07 09:12:53 +0100

    useState intro

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -11,12 +11,18 @@ interface IPropsExpensive {
 }
 
 const ExpenseItem = (props: IPropsExpensive) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button>Change Title</button>
       </div>
     </Card>
   );
