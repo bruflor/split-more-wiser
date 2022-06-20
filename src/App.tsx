9e36d070ad892b11d61e7395c1e56f43209d4a68
Bruna Flôr <brn.flor@gmail.com>
2022-06-20 20:13:36 +0100

    New Expenses

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <div>I'm not dummy</div>
+      <NewExpense />
       <Expenses />
     </div>
   );
