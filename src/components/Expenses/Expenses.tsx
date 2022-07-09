9b951c4ddf1da24ecf177fe29083d34a7c8ed8e6
Bruna Flôr <brn.flor@gmail.com>
2022-07-09 19:35:30 +0100

    expenses dinamic list

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -1,32 +1,11 @@
-import React, { useState } from "react";
+import React, { ReactComponentElement, useState } from "react";
 
 import Card from "../UI/Card";
 import { ExpensesFilter } from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
-const Expenses = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const Expenses = (props: any) => {
   const years = ["2022", "2021", "2020", "2019"];
   const [filterYear, setFilteryear] = useState(years[0]);
 
@@ -43,7 +22,7 @@ const Expenses = () => {
         handle={(event) => onSelected(event)}
       />
 
-      {expenses.map((expense) => {
+      {props.items.map((expense: any) => {
         return (
           <ExpenseItem
             id={expense.id}
@@ -54,30 +33,6 @@ const Expenses = () => {
           />
         );
       })}
-      {/* <ExpenseItem
-        id={expenses[0].id}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        id={expenses[1].id}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        id={expenses[2].id}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        id={expenses[3].id}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      /> */}
     </Card>
   );
 };
